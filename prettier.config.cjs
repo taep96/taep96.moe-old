72dbd4d90424d5ff86e133b54f59935085310034
taep96 <64481039+taep96@users.noreply.github.com>
2022-12-18 01:13:57 +0100

    initial project setup

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+// https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31
+
+module.exports = {
+    printWidth: 120,
+
+    pluginSearchDirs: false,
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+};
---
+++ stylelint.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-idiomatic-order",
+        "stylelint-config-prettier",
+        "stylelint-config-html",
+    ],
+    ignoreFiles: ["**/*.d.ts"],
+};
