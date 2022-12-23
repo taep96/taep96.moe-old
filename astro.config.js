b1b3b731f38568e2e8232d3727a6dbc903b35fef
taep96 <64481039+taep96@users.noreply.github.com>
2022-12-23 19:33:09 +0100

    add @astrojs/sitemap

--- astro.config.js
+++ astro.config.js
@@ -1,13 +1,16 @@
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://taep96.moe",
     integrations: [
         tailwind(),
         image({
             serviceEntryPoint: "@astrojs/image/sharp",
         }),
+        sitemap(),
     ],
 });
