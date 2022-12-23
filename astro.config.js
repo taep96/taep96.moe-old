a52cb53ab3a317bcfcbe2b0c7b6492f6c10d0446
taep96 <64481039+taep96@users.noreply.github.com>
2022-12-23 18:27:43 +0100

    add @astrojs/image

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,13 @@
+import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [
+        tailwind(),
+        image({
+            serviceEntryPoint: "@astrojs/image/sharp",
+        }),
+    ],
 });
