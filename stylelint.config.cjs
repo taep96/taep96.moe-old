f15ebb4c2d832c3f137bb34e07fd0dd1560f4483
taep96 <64481039+taep96@users.noreply.github.com>
2023-01-03 18:51:45 +0100

    stylelint = bloat

--- stylelint.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: [
-        "stylelint-config-standard",
-        "stylelint-config-idiomatic-order",
-        "stylelint-config-prettier",
-        "stylelint-config-html",
-    ],
-    ignoreFiles: ["**/*.d.ts"],
-};
